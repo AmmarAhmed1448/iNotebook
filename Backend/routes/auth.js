6ca6ef57fffd190f341725b9132137180dae0d79
AmmarAhmed1448 <ammarahmed1448@gmail.com>
2023-09-23 00:33:13 +0500

    Data validation and data duplication checking logic implemented

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,12 +1,43 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User")
+const { body, validationResult } = require('express-validator');
+const User = require("../models/User");
 
-router.post("/", (req, res) => {
+router.post("/", 
+[
+    body("name", "Enter a name").notEmpty(),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Enter a valid password").isLength({min:10}),
+    // Check if the email is unique
+    body("email").custom(async (value) => {
+        const user = await User.findOne({ email: value });
+        if (user) {
+            throw new Error('Email is already in use');
+        }
+    })
+
+]
+, (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      
     console.log(req.body);
-    const user = User(req.body);
-    res.send(req.body);
-    user.save();
+    const user = new User(req.body);
+    // res.send(req.body);
+    // user.save();
+
+    // Does the same job as newUser() + user.save()
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+
+    }).then(user => res.json(user))
+    .catch(err => res.json({error: err}));
+
 });
 
 module.exports = router;
